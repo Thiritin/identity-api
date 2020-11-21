985049527f7bfc3cbff39613a37d2de12c5f8001
Thiritin <kontakt@martin-becker.ovh>
2020-11-21 23:51:14 +0100

    ide:helper + add Group

---
+++ app/Models/Group.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Stride Authentication Backend
+ *
+ * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
+ * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
+ * @link		https://stride.thiritin.com
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Jamesh\Uuid\HasUuid;
+
+/**
+ * App\Models\Group
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $logo
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Collection|User[] $users
+ * @property-read int|null $users_count
+ * @method static Builder|Group newModelQuery()
+ * @method static Builder|Group newQuery()
+ * @method static Builder|Group query()
+ * @method static Builder|Group whereCreatedAt($value)
+ * @method static Builder|Group whereId($value)
+ * @method static Builder|Group whereLogo($value)
+ * @method static Builder|Group whereName($value)
+ * @method static Builder|Group whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
+class Group extends Model
+{
+    use HasFactory, HasUuid;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
+
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
