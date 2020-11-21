2db02622d650c879212650874b75d583bd5f54e1
Thiritin <me@thiritin.com>
2020-11-21 23:51:14 +0100

    ide:helper + add Group

--- app/Models/TwoFactor.php
+++ app/Models/TwoFactor.php
@@ -10,18 +10,33 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Jamesh\Uuid\HasUuid;
 
 /**
+ * App\Models\TwoFactor
+ *
  * @property string $id
  * @property string $user_id
  * @property string $secret
  * @property string $type
  * @property Carbon $created_at
  * @property Carbon $updated_at
+ * @property-read User $user
+ * @method static Builder|TwoFactor newModelQuery()
+ * @method static Builder|TwoFactor newQuery()
+ * @method static Builder|TwoFactor query()
+ * @method static Builder|TwoFactor whereCreatedAt($value)
+ * @method static Builder|TwoFactor whereId($value)
+ * @method static Builder|TwoFactor whereSecret($value)
+ * @method static Builder|TwoFactor whereType($value)
+ * @method static Builder|TwoFactor whereUpdatedAt($value)
+ * @method static Builder|TwoFactor whereUserId($value)
+ * @mixin Eloquent
  */
 class TwoFactor extends Model
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,15 +10,23 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Jamesh\Uuid\HasUuid;
+use Spatie\Activitylog\Models\Activity;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 /**
+ * App\Models\User
+ *
  * @property string $id
  * @property string $name
  * @property string $email
@@ -28,6 +36,29 @@ use Spatie\Permission\Traits\HasRoles;
  * @property string $remember_token
  * @property Carbon $created_at
  * @property Carbon $updated_at
+ * @property-read Collection|Activity[] $activities
+ * @property-read int|null $activities_count
+ * @property-read Collection|Permission[] $permissions
+ * @property-read int|null $permissions_count
+ * @property-read Collection|Role[] $roles
+ * @property-read int|null $roles_count
+ * @property-read Collection|TwoFactor[] $twoFactors
+ * @property-read int|null $two_factors_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User permission($permissions)
+ * @method static Builder|User query()
+ * @method static Builder|User role($roles, $guard = null)
+ * @method static Builder|User whereAvatar($value)
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class User extends Authenticatable
 {
