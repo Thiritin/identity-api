3dcf5cdca4a18740d269fdd066e378f99b602cad
Thiritin <me@thiritin.com>
2020-11-22 04:53:17 +0100

    Revert "Fix Copyright Typo and update Link"
    This reverts commit 3b02b527

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,14 +7,6 @@
  * @link		https://github.com/Thiritin/strive
  */
 
-/*
- * Strive Authentication Backend
- *
- * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
- * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
- * @link		https://github.com/Thiritin/strive
- */
-
 namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,14 +8,6 @@
  */
 
 /*
- * Strive Authentication Backend
- *
- * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
- * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
- * @link		https://github.com/Thiritin/strive
- */
-
-/*
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
