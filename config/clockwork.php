f6b67dfbb40463e5f5e3242d50a74a746abae7a5
Thiritin <me@thiritin.com>
2020-11-22 04:49:55 +0100

    Fix Copyright Typo and update Link

--- config/clockwork.php
+++ config/clockwork.php
@@ -1,10 +1,18 @@
 <?php
 /*
- * Stride Authentication Backend
+ * Strive Authentication Backend
  *
  * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
  * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
- * @link		https://stride.thiritin.com
+ * @link		https://github.com/Thiritin/strive
+ */
+
+/*
+ * Strive Authentication Backend
+ *
+ * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
+ * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
+ * @link		https://github.com/Thiritin/strive
  */
 
 use Illuminate\Container\Container;
