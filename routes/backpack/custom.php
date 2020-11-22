068553ba05802f3ab2237bc125fa4f1ca2314a9f
Thiritin <me@thiritin.com>
2020-11-22 04:44:26 +0100

    Added Backpack

---
+++ routes/backpack/custom.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Stride Authentication Backend
+ *
+ * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
+ * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
+ * @link		https://stride.thiritin.com
+ */
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group(
+    [
+        'prefix' => config('backpack.base.route_prefix', 'admin'),
+        'middleware' => array_merge(
+            (array)config('backpack.base.web_middleware', 'web'),
+            (array)config('backpack.base.middleware_key', 'admin')
+        ),
+        'namespace' => 'App\Http\Controllers\Admin',
+    ],
+    function () { // custom admin routes
+    }
+); // this should be the absolute last line of this file
