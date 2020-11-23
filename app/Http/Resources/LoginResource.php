f7e548f4c665e701245b3ec0cdccc296d7eb6a55
Thiritin <me@thiritin.com>
2020-11-23 04:04:37 +0100

    Reformat using Laravel Standard

--- app/Http/Resources/LoginResource.php
+++ app/Http/Resources/LoginResource.php
@@ -22,7 +22,7 @@ class LoginResource extends JsonResource
     }
 
     /**
-     * @param Request $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
