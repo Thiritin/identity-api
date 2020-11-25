33e719752b3ce38c7cf0e790cd98ba433d91ad5a
Thiritin <kontakt@martin-becker.ovh>
2020-11-25 20:10:26 +0100

    Admin v1

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Eurofurence Identity Provider Authentication Backend
+ *
+ * @copyright	Copyright (c) 2020 Martin Becker (https://martin-becker.ovh)
+ * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
+ * @link		https://github.com/Thiritin/ef-idp
+ */
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email|max:255',
+            'roles' => '',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,20 +1,18 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i
-                class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i>
-        Logs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon la la-cog'></i> <span>Settings</span></a>
-</li>
+                class="fas fa-tachometer-alt nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon fas fa-user"></i>
+        <span>Users</span></a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('group') }}'><i class='nav-icon fas fa-users'></i>
+        Groups</a></li>
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fas fa-cogs"></i> System</a>
     <ul class="nav-dropdown-items">
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i>
-                <span>Users</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i
-                        class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i
-                        class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon'></i>
+                <span>Settings</span></a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon'></i>
+                Logs</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon'></i>
+                Backups</a></li>
     </ul>
 </li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon la la-hdd-o'></i>
-        Backups</a></li>
