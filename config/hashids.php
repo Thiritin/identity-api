1c1632b25affe6bcef3386eee7bcfdedfcdc6053
Thiritin <kontakt@martin-becker.ovh>
2021-01-24 00:23:46 +0100

    Add custom checklist and hashid config

---
+++ config/hashids.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Eurofurence Identity Provider Authentication Backend
+ *
+ * @copyright	Copyright (c) 2021 Martin Becker (https://martin-becker.ovh)
+ * @license		GNU AGPLv3 (GNU Affero General Public License v3.0)
+ * @link		https://github.com/Thiritin/ef-idp
+ */
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/laravel-hashids
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'salt' => env('HASHIDS_SALT'),
+            'length' => 16,
+            'alphabet' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+        ],
+    ],
+
+];
