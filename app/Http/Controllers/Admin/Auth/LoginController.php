3058b6bd1ea786249f2011a012b0f20f4b81ec0b
Thiritin <me@thiritin.com>
2021-02-22 21:06:21 +0100

    Added Identity

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -19,6 +19,7 @@ class LoginController extends Controller
             config('services.oidc.admin.client_id'),
             config('services.oidc.admin.secret')
         );
+        $oidc->addScope('openid','admin');
         /**
          * Only for development
          */
